b9797bd2feb35698e8c18ca01cdcfc8d57b6ff69
Alibabson <erykbroniewicz23@gmail.com>
2025-04-07 02:03:45 +0200

    formularz w wiekszosci zrobiony oraz kontakt

--- JavaScript/galeria.js
+++ JavaScript/galeria.js
@@ -5,8 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const lightbox_img = document.getElementById('lightbox-img');
     const zamknij = document.getElementById('close');
 
+    if(!lightbox)
+        throw new Error("Brak Lighboxa");
+    if(!lightbox_img)
+        throw new Error("Brak zdjęć do Lighboxa");
+    if(!zamknij)
+        throw new Error("Brak przycisku zamknięcia");
+
     galleryImages.forEach(img => {
         img.addEventListener('click', () => {
+            if(!img.dataset.full)
+                throw new Error("Brak prawidłowego zdjęcia do Lighboxa");
             lightbox_img.src = img.dataset.full;
             lightbox.classList.remove('hidden');
         });
--- JavaScript/przejscia.js
+++ JavaScript/przejscia.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const ukryte = document.querySelectorAll('.Section_hidden');
-
+    if (!ukryte) {
+        throw new Error("Brak sekcji do animacji");
+    }
     const observer = new IntersectionObserver((wejcia, observer) => {
         wejcia.forEach(wej => {
             if (wej.isIntersecting) {
