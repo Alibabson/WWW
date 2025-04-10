cc0511a65220f21a8c41b2770844af6dc3efa9cf
Alibabson <erykbroniewicz23@gmail.com>
2025-04-10 22:06:50 +0200

    DarkMode dla form
    + poprawa kodu

--- JavaScript/przejscia.js
+++ JavaScript/przejscia.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const ukryte = document.querySelectorAll('.Section_hidden');
+    const ukryte = document.querySelectorAll('.Ukryte');
     if (!ukryte) {
         throw new Error("Brak sekcji do animacji");
     }
     const observer = new IntersectionObserver((wejcia, observer) => {
         wejcia.forEach(wej => {
             if (wej.isIntersecting) {
-                wej.target.classList.add('animate');
+                wej.target.classList.add('animacja');
                 observer.unobserve(wej.target);
             }
         });
