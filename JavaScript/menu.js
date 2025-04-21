4fd1cbac20dfedd1c77046cad231e2519957088c
Alibabson <erykbroniewicz23@gmail.com>
2025-04-21 14:35:19 +0200

    pprawione menu

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -1,4 +1,18 @@
-function toggleButtons() {
-    const buttons = document.querySelector('.dropdown-buttons');
-    buttons.classList.toggle('show');
+function Przelacz() {
+    const guzik = document.querySelector('.rozwin-guzik');
+    
+    if(guzik.classList.contains('pokaz')) {
+        guzik.classList.remove('pokaz');
+        guzik.classList.add('ukryj');
+
+        setTimeout(() => {
+            guzik.classList.remove('ukryj');
+            guzik.style.display = 'none';
+        }, 500);
+    }
+    else {
+        guzik.style.display = 'flex';
+        guzik.classList.remove('ukryj');
+        guzik.classList.add('pokaz');
+    }
 }
